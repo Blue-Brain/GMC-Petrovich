132dec24285d7bb55fa4cb7727af364e91ef7f9b
Blue-Brain <89130446037@yandex.ru>
2020-06-17 23:19:07 +0300

    make folder '/build' available for git

---
+++ build/precache-manifest.14aac322bbfe5f100b1f5b44132a801a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e52e3674b43a90630a42ebc86afa9fb4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "42522718a99536514528",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "87b55718dd5a208ea493",
+    "url": "./static/js/2.ac54c726.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.ac54c726.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42522718a99536514528",
+    "url": "./static/js/main.da8dd573.chunk.js"
+  },
+  {
+    "revision": "44b90b600c7ab6129ea2",
+    "url": "./static/js/runtime-main.a0cbdd31.js"
+  }
+]);
